8e89658fb6ad5a66a4029edb71b6bbb1c304c92e
Augustin GIRAUDIER <augiraudie>
2023-01-23 17:13:20 +0100

    ajout component card

---
+++ TPBlazorApp/TPBlazorApp/Pages/Index.razor.cs
@@ -0,0 +1,14 @@
+﻿using TPBlazorApp.Models;
+
+namespace TPBlazorApp.Pages
+{
+    public partial class Index
+    {
+        private Cake CakeItem = new Cake
+        {
+            Id = 1,
+            Name = "Black Forest",
+            Cost = 50
+        };
+    }
+}
