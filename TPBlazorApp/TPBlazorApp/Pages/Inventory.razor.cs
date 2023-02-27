9e110ad086f9135a9c97016da089a20c9fe7d156
Augustin Giraudier <giraudieraugustin@gmail.com>
2023-02-27 17:37:34 +0100

    nettoyage class Inventory.razor.cs

--- TPBlazorApp/TPBlazorApp/Pages/Inventory.razor.cs
+++ TPBlazorApp/TPBlazorApp/Pages/Inventory.razor.cs
@@ -11,25 +11,13 @@ namespace TPBlazorApp.Pages
 {
     public partial class Inventory
     {
-        [Inject]
-        public IStringLocalizer<List> Localizer { get; set; }
 
         private List<Item> items;
-
-        [Inject]
-        public NavigationManager NavigationManager { get; set; }
-
-        [CascadingParameter]
-        public IModalService Modal { get; set; }
-
 
         private int totalItem;
 
         [Inject]
         public IDataService DataService { get; set; }
-
-        [Inject]
-        public IWebHostEnvironment WebHostEnvironment { get; set; }
 
         private async Task OnReadData(DataGridReadDataEventArgs<Item> e)
         {
@@ -43,25 +31,6 @@ namespace TPBlazorApp.Pages
                 items = await DataService.List(e.Page, e.PageSize);
                 totalItem = await DataService.Count();
             }
-        }
-
-        private async void OnDelete(int id)
-        {
-            var parameters = new ModalParameters();
-            parameters.Add(nameof(Item.Id), id);
-
-            var modal = Modal.Show<DeleteConfirmation>("Delete Confirmation", parameters);
-            var result = await modal.Result;
-
-            if (result.Cancelled)
-            {
-                return;
-            }
-
-            await DataService.Delete(id);
-
-            // Reload the page
-            NavigationManager.NavigateTo("list", true);
         }
     }
 }
