c9647d65d47178c5a3da547d1c89d4bef0dbbcfc
Augustin Giraudier <giraudieraugustin@gmail.com>
2023-02-28 18:49:25 +0100

    adding trash

---
+++ TPBlazorApp/TPBlazorApp/Components/ItemTrash.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TPBlazorApp.Components
+{
+    public partial class ItemTrash
+    {
+
+        [CascadingParameter]
+        public InventoryManager Parent { get; set; }
+
+        internal void OnDrop()
+        {
+            Parent.CurrentDragSlot.reset();
+        }
+    }
+}
